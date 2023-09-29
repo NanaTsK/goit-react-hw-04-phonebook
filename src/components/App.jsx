c4224844025c3af92aabfdee754d1fb07850f91b
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-29 15:28:47 +0200

    switched to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -97,3 +97,81 @@ export const App = () => {
     </Container>
   );
 };
+
+//* ====== Old code before transitioning to hooks from class components. ======
+// import { Component } from 'react';
+// import { Container } from './index.styled';
+// import { ContactForm } from './ContactForm/ContactForm';
+// import { ContactList } from './ContactList/ContactList';
+// import { Filter } from './Filter/Filter';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// const notifyInit = Notify.init({
+//   width: '280px',
+//   position: 'center-center',
+//   distance: '20px',
+//   opacity: 0.8,
+//   fontSize: '20px',
+//   borderRadius: '50px 10px',
+//   notiflixIconColor: 'rgba(0,0,0,0.6)',
+//   pauseOnHover: true,
+// });
+
+// export class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Lesya Ukrainka', number: '459-12-56' },
+//       { id: 'id-2', name: 'Boris JohnsonUK', number: '443-89-12' },
+//       { id: 'id-3', name: 'Taras Shevchenko', number: '645-17-79' },
+//       { id: 'id-4', name: 'Crimea BeachClub', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
+
+//   handleAddContact = newContact => {
+//     const isTrue = this.state.contacts.some(
+//       ({ name }) => name === newContact.name
+//     );
+//     if (isTrue) {
+//       Notify.info(
+//         `${newContact.name} is already in your Phonebook!`,
+//         notifyInit
+//       );
+//       return;
+//     }
+//     this.setState(({ contacts }) => ({ contacts: [newContact, ...contacts] }));
+//     Notify.success(`${newContact.name} added to your Phonebook!`, notifyInit);
+//   };
+
+//   getFilterContacts = () => {
+//     const { contacts, filter } = this.state;
+//     return contacts.filter(({ name }) =>
+//       name.toLowerCase().includes(filter.toLowerCase().trim())
+//     );
+//   };
+//   removeContact = id => {
+//     this.setState(prev => ({
+//       contacts: prev.contacts.filter(contact => contact.id !== id),
+//     }));
+//   };
+
+//   handleFilter = ({ target: { value } }) => {
+//     this.setState({ filter: value });
+//   };
+
+//   render() {
+//     const filterContacts = this.getFilterContacts();
+//     return (
+//       <Container>
+//         <h1>Phonebook</h1>
+//         <ContactForm handleAddContact={this.handleAddContact} />
+//         <h2>Contacts</h2>
+//         <Filter filter={this.state.filter} handleFilter={this.handleFilter} />
+//         <ContactList
+//           contacts={filterContacts}
+//           removeContact={this.removeContact}
+//         />
+//       </Container>
+//     );
+//   }
+// }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -80,3 +80,78 @@ export const ContactForm = ({ handleAddContact }) => {
     </PhonebookForm>
   );
 };
+
+//* ====== Old code before transitioning to hooks from class components. ======
+// import { Component } from 'react';
+// import { nanoid } from 'nanoid';
+// import {
+//   PhonebookForm,
+//   InputWrap,
+//   ContactLabel,
+//   ContactInput,
+//   ContactAddBtn,
+// } from './ContactForm.styled';
+
+// const INITIAL_STATE = {
+//   name: '',
+//   number: '',
+// };
+
+// export class ContactForm extends Component {
+//   state = INITIAL_STATE;
+
+//   handleInput = e => {
+//     this.setState({
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+
+//   handleSubmitForm = e => {
+//     e.preventDefault();
+//     const newContact = {
+//       id: nanoid(),
+//       name: this.state.name,
+//       number: this.state.number,
+//     };
+//     this.props.handleAddContact(newContact, this.state);
+//     this.setState(INITIAL_STATE);
+//   };
+
+//   render() {
+//     return (
+//       <PhonebookForm onSubmit={this.handleSubmitForm}>
+//         <InputWrap>
+//           <ContactInput
+//             type="text"
+//             name="name"
+//             pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Name may contain up to 16 letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Comte d'Artagnan"
+//             maxLength="16"
+//             autoComplete="off"
+//             value={this.state.name}
+//             onChange={this.handleInput}
+//             required
+//           />
+//           <ContactLabel>Name</ContactLabel>
+//         </InputWrap>
+
+//         <InputWrap>
+//           <ContactInput
+//             type="tel"
+//             name="number"
+//             pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+//             title="Phone number may contain 5-16 digits, spaces, dashes, parentheses and can start with +"
+//             maxLength="16"
+//             autoComplete="off"
+//             value={this.state.number}
+//             onChange={this.handleInput}
+//             required
+//           />
+//           <ContactLabel>Number</ContactLabel>
+//         </InputWrap>
+
+//         <ContactAddBtn type="submit">Add contact</ContactAddBtn>
+//       </PhonebookForm>
+//     );
+//   }
+// }
