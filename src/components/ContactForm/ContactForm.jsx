5957d80ca3c6a354f09d80a45a14edf4ed410dcf
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-29 10:11:06 +0200

    switched to hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import {
   PhonebookForm,
@@ -13,62 +13,70 @@ const INITIAL_STATE = {
   number: '',
 };
 
-export class ContactForm extends Component {
-  state = INITIAL_STATE;
+export const ContactForm = ({ handleAddContact }) => {
+  const [name, setName] = useState(INITIAL_STATE.name);
+  const [number, setNumber] = useState(INITIAL_STATE.number);
 
-  handleInput = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+  const handleInput = e => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleSubmitForm = e => {
+  const handleSubmitForm = e => {
     e.preventDefault();
     const newContact = {
       id: nanoid(),
-      name: this.state.name,
-      number: this.state.number,
+      name: name,
+      number: number,
     };
-    this.props.handleAddContact(newContact, this.state);
-    this.setState(INITIAL_STATE);
+    handleAddContact(newContact);
+    setName(INITIAL_STATE.name);
+    setNumber(INITIAL_STATE.number);
   };
 
-  render() {
-    return (
-      <PhonebookForm onSubmit={this.handleSubmitForm}>
-        <InputWrap>
-          <ContactInput
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            // pattern="^[a-zA-Zа-яА-Я]+([' \-]?[a-zA-Zа-яА-Я]+)*$"
-            title="Name may contain up to 16 letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Comte d'Artagnan"
-            maxLength="16"
-            autoComplete="off"
-            value={this.state.name}
-            onChange={this.handleInput}
-            required
-          />
-          <ContactLabel>Name</ContactLabel>
-        </InputWrap>
+  return (
+    <PhonebookForm onSubmit={handleSubmitForm}>
+      <InputWrap>
+        <ContactInput
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain up to 16 letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Comte d'Artagnan"
+          maxLength="16"
+          autoComplete="off"
+          value={name}
+          onChange={handleInput}
+          required
+        />
+        <ContactLabel>Name</ContactLabel>
+      </InputWrap>
 
-        <InputWrap>
-          <ContactInput
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-            title="Phone number may contain 5-16 digits, spaces, dashes, parentheses and can start with +"
-            maxLength="16"
-            autoComplete="off"
-            value={this.state.number}
-            onChange={this.handleInput}
-            required
-          />
-          <ContactLabel>Number</ContactLabel>
-        </InputWrap>
+      <InputWrap>
+        <ContactInput
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+          title="Phone number may contain 5-16 digits, spaces, dashes, parentheses and can start with +"
+          maxLength="16"
+          autoComplete="off"
+          value={number}
+          onChange={handleInput}
+          required
+        />
+        <ContactLabel>Number</ContactLabel>
+      </InputWrap>
 
-        <ContactAddBtn type="submit">Add contact</ContactAddBtn>
-      </PhonebookForm>
-    );
-  }
-}
+      <ContactAddBtn type="submit">Add contact</ContactAddBtn>
+    </PhonebookForm>
+  );
+};
